ec88ba3fd7f70b08498e9f8f629b05e8fd59e865
anna <anna>
2022-04-12 23:38:55 +0200

    updated with logo's location

--- fe_challenge/src/components/SortingButton.js
+++ fe_challenge/src/components/SortingButton.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import stylesSortingButton from '../styles/SortingButton.module.css'
-import {BrowserView} from 'react-device-detect'
 
 export default function SortingButton(props) {
   return (
@@ -8,7 +7,6 @@ export default function SortingButton(props) {
       <div className={stylesSortingButton['totalNumber']}>
         All jobs - {props.numberOfSearchedJobs > 0 ? props.numberOfSearchedJobs : props.noJobs ? 0 : props.numberOfJobs} results
       </div>
-      <BrowserView>
         <div className={stylesSortingButton['sortingButton']}>
           Sort results by
           <select className={stylesSortingButton['selectButton']}>
@@ -16,8 +14,6 @@ export default function SortingButton(props) {
             <option> Relevance</option>
           </select>
         </div>
-      </BrowserView>
-      
     </div>
   )
 }
